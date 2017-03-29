37b7bdd8b3630707d44cd0037e2560bc1a3f2357
Jose María Tomé <jmtome@ceinpro.es>
2017-03-29 21:30:59 +0200

    Inicio

--- yuyu/src/app/app.component.js
+++ yuyu/src/app/app.component.js
@@ -8,15 +8,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = "Ranking de jug.";
         this.name = 'Mundo, qué hacemos';
+        this.player = {
+            id: 1,
+            name: "Braulio",
+            presentacion: presentacion
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n  <h1>Hola {{name}}</h1>\n  <p> desconocido us\u00F3 una galer\u00EDa de textos y los mezcl\u00F3 de tal manera que logr\u00F3 hacer un libro de textos especimen.</p>\n  \n  ",
+        template: "\n  <h1>{{title}}</h1>\n  <h1>Hola {{name}}</h1>\n  <h2>Hola {{player}}</h2>\n  <p> desconocido us\u00F3 una galer\u00EDa de textos y los mezcl\u00F3 de tal manera que logr\u00F3 hacer un libro de textos especimen.</p>\n  \n  ",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var Jugador = (function () {
+    function Jugador() {
+    }
+    return Jugador;
+}());
+exports.Jugador = Jugador;
 //# sourceMappingURL=app.component.js.map
--- yuyu/src/app/app.component.ts
+++ yuyu/src/app/app.component.ts
@@ -3,10 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+  <h1>{{title}}</h1>
   <h1>Hola {{name}}</h1>
+  <h2>Hola {{player}}</h2>
   <p> desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
   
   `,
 })
+
+
+
 //AppComponent es la clase que representa a este componente
-export class AppComponent  { name = 'Mundo, qué hacemos'; }
+export class AppComponent  { 
+title = "Ranking de jug."
+name = 'Mundo, qué hacemos';
+    player : Jugador={
+    id:1,
+    name:"Braulio",
+    presentacion ="Me presento, hale."
+}
+
+
+}
+
+export class Jugador{
+id:number;
+name:string;
+presentacion:string;
+}
